1294222b07485315147b6210cc78cecaf689d6d8
Alexander Olshanskiy <sander2411ukr@gmail.com>
2020-06-21 21:33:52 +0300

    update reducer

--- src/actions/types.ts
+++ src/actions/types.ts
@@ -1,4 +1,7 @@
+import { FetchToDosAction, DeleteToDoAction } from './';
 export enum ActionTypes {
   FETCH_TO_DOS_SUCCESS = 'FETCH_TO_DOS_SUCCESS',
   DELETE_TO_DO = 'DELETE_TO_DO',
 }
+
+export type Action = FetchToDosAction | DeleteToDoAction;
--- src/reducers/toDos.ts
+++ src/reducers/toDos.ts
@@ -1,12 +1,18 @@
-import { ToDo, FetchToDosAction, ActionTypes } from '../actions';
+import { ToDo, Action, ActionTypes } from '../actions';
+import { stat } from 'fs';
 
 
-export const toDosReducer = (state: ToDo[] = [], action: FetchToDosAction): ToDo[] => {
+
+export const toDosReducer = (state: ToDo[] = [], action: Action): ToDo[] => {
 
   switch (action.type) {
     case ActionTypes.FETCH_TO_DOS_SUCCESS:
       state = action.payload;
       return state;
+
+    case ActionTypes.DELETE_TO_DO:
+      // DON'T DO THAT!!!
+      return state.filter((todo: ToDo) => todo.id !== action.payload);
     default:
       return state;
   }
