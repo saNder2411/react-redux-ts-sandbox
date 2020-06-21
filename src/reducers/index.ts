f6c6c00a7e545fb4cbd1f6227c539f95b3f53d36
Alexander Olshanskiy <sander2411ukr@gmail.com>
2020-06-21 19:32:58 +0300

    validate store

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,5 +1,11 @@
 import { combineReducers } from 'redux';
+import { toDosReducer } from './toDos';
+import { ToDo } from './../actions';
 
-export const reducers = combineReducers({
-  counter: () => 1
+interface StoreState {
+  toDos: ToDo[],
+}
+
+export const reducers = combineReducers<StoreState>({
+  toDos: toDosReducer,
 });
--- src/reducers/toDos.ts
+++ src/reducers/toDos.ts
@@ -3,7 +3,7 @@ import { ActionTypes } from '../actions/types'
 
 
 
-export const toDosReducer = (state: ToDo[] = [], action: FetchToDosAction) => {
+export const toDosReducer = (state: ToDo[] = [], action: FetchToDosAction): ToDo[] => {
 
   switch (action.type) {
     case ActionTypes.FETCH_TO_DOS_SUCCESS:
